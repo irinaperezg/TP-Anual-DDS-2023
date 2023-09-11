9e1a856d43aeb0d0893bf5d882914a5212316fbe
irinaperezg <iprezgribnicow@frba.utn.edu.ar>
2023-09-11 16:12:32 -0300

    persisto notificacion

--- src/main/java/domain/main/notificaciones/frecuenciasNotificacion/Notificacion.java
+++ src/main/java/domain/main/notificaciones/frecuenciasNotificacion/Notificacion.java
@@ -1,5 +1,6 @@
 package domain.main.notificaciones.frecuenciasNotificacion;
 
+import domain.converter.LocalDateAttributeConverter;
 import domain.main.Establecimiento;
 import domain.main.incidentes.Incidente;
 import domain.main.servicio.Servicio;
@@ -7,15 +8,33 @@ import domain.usuarios.Persona;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 @Getter
+@Entity
 public class Notificacion {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "persona_id", referencedColumnName = "id")
   private Persona destinatario;
+
   @Setter
+  @Column(name = "mensaje")
   private String mensaje;
+
+  @Enumerated(EnumType.STRING)
   private EstadoNotificacion estado;
+
+  @Convert(converter = LocalDateAttributeConverter.class)
+  @Column(name="fechaAperturaIncidente")
   private LocalDateTime fechaAperturaIncidente;
+  @Convert(converter = LocalDateAttributeConverter.class)
+  @Column(name="fechaCierreIncidente")
   private LocalDateTime fechaCierreIncidente;
 
   public Notificacion (Persona destinatario, Incidente incidente) {
@@ -24,6 +43,10 @@ public class Notificacion {
     estado = EstadoNotificacion.PENDIENTE_A_ENVIAR;
     fechaAperturaIncidente = incidente.getFechaApertura();
     fechaCierreIncidente = incidente.getFechaCierre();
+  }
+
+  public Notificacion() {
+
   }
 
   public String generarMensaje(Incidente incidente) {
