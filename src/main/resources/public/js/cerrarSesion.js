89a5defbfec29a35a539de2905ce24ea95f51673
Chabelamas <83524699+Chabelamas@users.noreply.github.com>
2023-10-17 21:43:00 -0300

    arreglo cerrar sesion y sumo icono pagina head

---
+++ src/main/resources/public/js/cerrarSesion.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cerrarSesionLink = document.getElementById("cerrarSesionLink");
+
+    cerrarSesionLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita que el enlace se siga al hacer clic
+
+        // Muestra un cuadro de diálogo de confirmación
+        const confirmacion = confirm("¿Seguro que deseas cerrar la sesión?");
+
+        if (confirmacion) {
+            // Si el usuario confirma, redirige a la página de cierre de sesión
+            window.location.href = "/logout"; // Reemplaza con la URL correcta
+        }
+    });
+});
+
