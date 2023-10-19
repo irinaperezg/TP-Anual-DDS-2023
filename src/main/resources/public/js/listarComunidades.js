91b715840a93f18b7e6cca25efef3344a1190a34
Chabelamas <83524699+Chabelamas@users.noreply.github.com>
2023-10-18 21:16:50 -0300

    cambie una letra de un aviso

--- src/main/resources/public/js/listarComunidades.js
+++ src/main/resources/public/js/listarComunidades.js
@@ -6,7 +6,7 @@ var eliminarComunidadLink = document.getElementById('eliminarComunidad');
 // Agrega un evento clic al enlace
 eliminarComunidadLink.addEventListener('click', function (event) {
 // Pregunta al usuario si realmente desea eliminar la comunidad
-var confirmacion = confirm('¿Estás seguro de que deseas eliminar esta comunidad?');
+var confirmacion = confirm('¿Estás seguro/a de que deseas eliminar esta comunidad?');
 
 // Si el usuario cancela la acción, detén el enlace
 if (!confirmacion) {
