561a29ab7ce27e472cbbb405e5e9b8be62a3035e
irinaperezg <iprezgribnicow@frba.utn.edu.ar>
2023-11-30 01:16:46 -0300

    STACK TECNOLOGICO TERMINADOOOOOOOOO

--- nuevoStack/nuevoStack/incidentes/views.py
+++ nuevoStack/nuevoStack/incidentes/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Incidente
 
 def listar_incidentes(request):
+
     incidentes = Incidente.objects.all()
-    print(incidentes)
     return render(request, 'listar_incidentes.html', {'incidentes': incidentes})
--- nuevoStackTecnologico/nuevoStackTecnologico/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import listar_incidentes
-
-urlpatterns = [
-    path('incidentes/', listar_incidentes)
-]
