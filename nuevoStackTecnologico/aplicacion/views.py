748371d837d93ace067f17207ec35603aa624a4d
LucianoSangroni123 <102642306+LucianoSangroni123@users.noreply.github.com>
2023-11-30 00:47:11 -0300

    Delete nuevoStackTecnologico directory

--- nuevoStackTecnologico/aplicacion/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Incidente
-
-def listar_incidentes(request):
-    incidentes = Incidente.objects.all()
-    print(incidentes)
-    return render(request, 'listar_incidentes.html', {'incidentes': incidentes})
